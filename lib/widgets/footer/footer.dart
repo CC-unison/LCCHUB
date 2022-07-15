8bc99e6df07fc069fdee28051dd034a468ce976c
JesusUrias <jesusuriasp@gmail.com>
2022-07-15 10:55:51 -0700

    Corrección de warnings (const)

--- lib/widgets/footer/footer.dart
+++ lib/widgets/footer/footer.dart
@@ -9,13 +9,13 @@ class Footer extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
-      color: Color.fromARGB(255, 31, 41, 55),
+      color: const Color.fromARGB(255, 31, 41, 55),
       child: Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
         Column(children: [
           Column(
             children: [
               Row(
-                children: [
+                children: const [
                   Icon(Icons.phone,
                       color: Color.fromARGB(255, 105, 144, 235), size: 20),
                   Text("Contacto",
@@ -25,7 +25,7 @@ class Footer extends StatelessWidget {
                 ],
               ),
               Row(
-                children: [
+                children: const [
                   Text("Tel y Fax",
                       textAlign: TextAlign.end,
                       style: TextStyle(
@@ -34,7 +34,7 @@ class Footer extends StatelessWidget {
                 ],
               ),
               Row(
-                children: [
+                children: const [
                   Text("(662) 259.2155",
                       textAlign: TextAlign.end,
                       style: TextStyle(
@@ -43,7 +43,7 @@ class Footer extends StatelessWidget {
                 ],
               ),
               Row(
-                children: [
+                children: const [
                   Text("Ext: 2482 y 2494",
                       textAlign: TextAlign.end,
                       style: TextStyle(
@@ -52,7 +52,7 @@ class Footer extends StatelessWidget {
                 ],
               ),
               Row(
-                children: [
+                children: const [
                   Icon(Icons.wb_twilight_outlined,
                       color: Color.fromARGB(255, 105, 144, 235), size: 40),
                   Icon(Icons.facebook,
@@ -65,7 +65,7 @@ class Footer extends StatelessWidget {
         Column(
           children: [
             Row(
-              children: [
+              children: const [
                 Icon(Icons.location_on,
                     color: Color.fromARGB(255, 105, 144, 235), size: 20),
                 Text("Dirección",
@@ -77,7 +77,7 @@ class Footer extends StatelessWidget {
             Row(
               crossAxisAlignment: CrossAxisAlignment.start,
               mainAxisAlignment: MainAxisAlignment.start,
-              children: [
+              children: const [
                 Text(
                     "Edificio 3K4, Ciencias\nde la computación,\nBoulevard Luis\nEncinas y Rosales s/n\n, Col.Centro,\n,Hermosillo,Sonora.\n. CP 83000",
                     textAlign: TextAlign.end,
@@ -91,7 +91,7 @@ class Footer extends StatelessWidget {
         Column(
           children: [
             Row(
-              children: [
+              children: const [
                 Icon(Icons.mail,
                     color: Color.fromARGB(255, 105, 144, 235), size: 20),
                 Text("Correo",
@@ -103,7 +103,7 @@ class Footer extends StatelessWidget {
             Row(
               crossAxisAlignment: CrossAxisAlignment.start,
               mainAxisAlignment: MainAxisAlignment.start,
-              children: [
+              children: const [
                 Text("computacion@mat.uson.mx",
                     textAlign: TextAlign.end,
                     style: TextStyle(
