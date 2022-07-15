66ad6000586b671ee9324474d505c6675b45d196
HectorAgValenzuela <hector-av1@hotmail.com>
2022-07-15 13:10:36 -0700

    Se crea la clase Footer

--- lib/pages/invitado/invitado_page.dart
+++ lib/pages/invitado/invitado_page.dart
@@ -2,9 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:lcchub/config/responsive.dart';
 import 'package:lcchub/config/size_config.dart';
 import 'package:lcchub/widgets/buttons/primary_buttons.dart';
-import 'package:carousel_slider/carousel_slider.dart';
 
 import '../../widgets/cards/card.dart';
+import '../../widgets/footer/footer.dart';
 import '../../widgets/header/header.dart';
 
 class InvitadoPage extends StatefulWidget {
@@ -140,121 +140,7 @@ class _InvitadoPageState extends State<InvitadoPage> {
                     const Icon(Icons.image, size: 120)
                 ]),
           ),
-          Container(
-            width: double.infinity,
-            color: const Color.fromARGB(255, 31, 41, 55),
-            child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  Column(children: [
-                    Column(
-                      children: [
-                        Row(
-                          children: const [
-                            Icon(Icons.phone,
-                                color: Color.fromARGB(255, 105, 144, 235),
-                                size: 20),
-                            Text("Contacto",
-                                textAlign: TextAlign.end,
-                                style: TextStyle(
-                                    color: Color.fromARGB(255, 105, 144, 235))),
-                          ],
-                        ),
-                        Row(
-                          children: const [
-                            Text("Tel y Fax",
-                                textAlign: TextAlign.end,
-                                style: TextStyle(
-                                    color: Color.fromARGB(255, 255, 255, 255),
-                                    fontSize: 10)),
-                          ],
-                        ),
-                        Row(
-                          children: const [
-                            Text("(662) 259.2155",
-                                textAlign: TextAlign.end,
-                                style: TextStyle(
-                                    color: Color.fromARGB(255, 255, 255, 255),
-                                    fontSize: 10)),
-                          ],
-                        ),
-                        Row(
-                          children: const [
-                            Text("Ext: 2482 y 2494",
-                                textAlign: TextAlign.end,
-                                style: TextStyle(
-                                    color: Color.fromARGB(255, 255, 255, 255),
-                                    fontSize: 10)),
-                          ],
-                        ),
-                        Row(
-                          children: const [
-                            Icon(Icons.wb_twilight_outlined,
-                                color: Color.fromARGB(255, 105, 144, 235),
-                                size: 40),
-                            Icon(Icons.facebook,
-                                color: Color.fromARGB(255, 105, 144, 235),
-                                size: 40)
-                          ],
-                        ),
-                      ],
-                    )
-                  ]),
-                  Column(
-                    children: [
-                      Row(
-                        children: const [
-                          Icon(Icons.location_on,
-                              color: Color.fromARGB(255, 105, 144, 235),
-                              size: 20),
-                          Text("Dirección",
-                              textAlign: TextAlign.end,
-                              style: TextStyle(
-                                  color: Color.fromARGB(255, 105, 144, 235))),
-                        ],
-                      ),
-                      Row(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: const [
-                          Text(
-                              "Edificio 3K4, Ciencias\nde la computación,\nBoulevard Luis\nEncinas y Rosales s/n\n, Col.Centro,\n,Hermosillo,Sonora.\n. CP 83000",
-                              textAlign: TextAlign.end,
-                              style: TextStyle(
-                                  color: Color.fromARGB(255, 255, 255, 255),
-                                  fontSize: 10)),
-                        ],
-                      ),
-                    ],
-                  ),
-                  Column(
-                    children: [
-                      Row(
-                        children: const [
-                          Icon(Icons.mail,
-                              color: Color.fromARGB(255, 105, 144, 235),
-                              size: 20),
-                          Text("Correo",
-                              textAlign: TextAlign.end,
-                              style: TextStyle(
-                                  color: Color.fromARGB(255, 105, 144, 235))),
-                        ],
-                      ),
-                      Row(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: const [
-                          Text("computacion@mat.uson.mx",
-                              textAlign: TextAlign.end,
-                              style: TextStyle(
-                                  color: Color.fromARGB(255, 255, 255, 255),
-                                  fontSize: 10)),
-                        ],
-                      ),
-                    ],
-                  ),
-                ]),
-          ),
+          Footer(),
         ]),
       ),
     );
--- lib/widgets/footer/footer.dart
+++ lib/widgets/footer/footer.dart
@@ -10,110 +10,118 @@ class Footer extends StatelessWidget {
     return Container(
       width: double.infinity,
       color: const Color.fromARGB(255, 31, 41, 55),
-      child: Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
-        Column(children: [
-          Column(
-            children: [
-              Row(
-                children: const [
-                  Icon(Icons.phone,
-                      color: Color.fromARGB(255, 105, 144, 235), size: 20),
-                  Text("Contacto",
-                      textAlign: TextAlign.end,
-                      style:
-                          TextStyle(color: Color.fromARGB(255, 105, 144, 235))),
-                ],
-              ),
-              Row(
-                children: const [
-                  Text("Tel y Fax",
-                      textAlign: TextAlign.end,
-                      style: TextStyle(
-                          color: Color.fromARGB(255, 255, 255, 255),
-                          fontSize: 10)),
-                ],
-              ),
-              Row(
-                children: const [
-                  Text("(662) 259.2155",
-                      textAlign: TextAlign.end,
-                      style: TextStyle(
-                          color: Color.fromARGB(255, 255, 255, 255),
-                          fontSize: 10)),
-                ],
-              ),
-              Row(
-                children: const [
-                  Text("Ext: 2482 y 2494",
-                      textAlign: TextAlign.end,
-                      style: TextStyle(
-                          color: Color.fromARGB(255, 255, 255, 255),
-                          fontSize: 10)),
-                ],
-              ),
-              Row(
-                children: const [
-                  Icon(Icons.wb_twilight_outlined,
-                      color: Color.fromARGB(255, 105, 144, 235), size: 40),
-                  Icon(Icons.facebook,
-                      color: Color.fromARGB(255, 105, 144, 235), size: 40)
-                ],
-              ),
-            ],
-          )
-        ]),
-        Column(
+      child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
-            Row(
-              children: const [
-                Icon(Icons.location_on,
-                    color: Color.fromARGB(255, 105, 144, 235), size: 20),
-                Text("Dirección",
-                    textAlign: TextAlign.end,
-                    style:
-                        TextStyle(color: Color.fromARGB(255, 105, 144, 235))),
+            Column(children: [
+              Column(
+                children: [
+                  Row(
+                    children: const [
+                      Icon(Icons.phone,
+                          color: Color.fromARGB(255, 105, 144, 235),
+                          size: 20),
+                      Text("Contacto",
+                          textAlign: TextAlign.end,
+                          style: TextStyle(
+                              color: Color.fromARGB(255, 105, 144, 235))),
+                    ],
+                  ),
+                  Row(
+                    children: const [
+                      Text("Tel y Fax",
+                          textAlign: TextAlign.end,
+                          style: TextStyle(
+                              color: Color.fromARGB(255, 255, 255, 255),
+                              fontSize: 10)),
+                    ],
+                  ),
+                  Row(
+                    children: const [
+                      Text("(662) 259.2155",
+                          textAlign: TextAlign.end,
+                          style: TextStyle(
+                              color: Color.fromARGB(255, 255, 255, 255),
+                              fontSize: 10)),
+                    ],
+                  ),
+                  Row(
+                    children: const [
+                      Text("Ext: 2482 y 2494",
+                          textAlign: TextAlign.end,
+                          style: TextStyle(
+                              color: Color.fromARGB(255, 255, 255, 255),
+                              fontSize: 10)),
+                    ],
+                  ),
+                  Row(
+                    children: const [
+                      Icon(Icons.wb_twilight_outlined,
+                          color: Color.fromARGB(255, 105, 144, 235),
+                          size: 40),
+                      Icon(Icons.facebook,
+                          color: Color.fromARGB(255, 105, 144, 235),
+                          size: 40)
+                    ],
+                  ),
+                ],
+              )
+            ]),
+            Column(
+              children: [
+                Row(
+                  children: const [
+                    Icon(Icons.location_on,
+                        color: Color.fromARGB(255, 105, 144, 235),
+                        size: 20),
+                    Text("Dirección",
+                        textAlign: TextAlign.end,
+                        style: TextStyle(
+                            color: Color.fromARGB(255, 105, 144, 235))),
+                  ],
+                ),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: const [
+                    Text(
+                        "Edificio 3K4, Ciencias\nde la computación,\nBoulevard Luis\nEncinas y Rosales s/n\n, Col.Centro,\n,Hermosillo,Sonora.\n. CP 83000",
+                        textAlign: TextAlign.end,
+                        style: TextStyle(
+                            color: Color.fromARGB(255, 255, 255, 255),
+                            fontSize: 10)),
+                  ],
+                ),
               ],
             ),
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: const [
-                Text(
-                    "Edificio 3K4, Ciencias\nde la computación,\nBoulevard Luis\nEncinas y Rosales s/n\n, Col.Centro,\n,Hermosillo,Sonora.\n. CP 83000",
-                    textAlign: TextAlign.end,
-                    style: TextStyle(
-                        color: Color.fromARGB(255, 255, 255, 255),
-                        fontSize: 10)),
+            Column(
+              children: [
+                Row(
+                  children: const [
+                    Icon(Icons.mail,
+                        color: Color.fromARGB(255, 105, 144, 235),
+                        size: 20),
+                    Text("Correo",
+                        textAlign: TextAlign.end,
+                        style: TextStyle(
+                            color: Color.fromARGB(255, 105, 144, 235))),
+                  ],
+                ),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: const [
+                    Text("computacion@mat.uson.mx",
+                        textAlign: TextAlign.end,
+                        style: TextStyle(
+                            color: Color.fromARGB(255, 255, 255, 255),
+                            fontSize: 10)),
+                  ],
+                ),
               ],
             ),
-          ],
-        ),
-        Column(
-          children: [
-            Row(
-              children: const [
-                Icon(Icons.mail,
-                    color: Color.fromARGB(255, 105, 144, 235), size: 20),
-                Text("Correo",
-                    textAlign: TextAlign.end,
-                    style:
-                        TextStyle(color: Color.fromARGB(255, 105, 144, 235))),
-              ],
-            ),
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: const [
-                Text("computacion@mat.uson.mx",
-                    textAlign: TextAlign.end,
-                    style: TextStyle(
-                        color: Color.fromARGB(255, 255, 255, 255),
-                        fontSize: 10)),
-              ],
-            ),
-          ],
-        ),
-      ]),
+          ]),
     );
   }
 }
+
