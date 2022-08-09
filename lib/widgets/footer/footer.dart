f2b74df47a1cda5b17f7227b3b801ac46d29b419
HectorAgValenzuela <hector-av1@hotmail.com>
2022-08-08 21:38:59 -0700

    crossAxisAlignment fixed

--- lib/widgets/footer/footer.dart
+++ lib/widgets/footer/footer.dart
@@ -126,100 +126,107 @@ class FooterWeb extends StatelessWidget {
       padding: const EdgeInsets.only(top: 45, bottom: 45),
       child: Container(
         margin: const EdgeInsets.only(left: 100, right: 100),
-        child: Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
-          Column(children: [
-            Column(
-              children: [
-                Container(
-                  margin: const EdgeInsets.only(right: 60),
-                  child: Row(
+        child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Column(children: [
+                Column(
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.only(right: 60),
+                      child: Row(
+                        children: const [
+                          Icon(Icons.phone,
+                              color: Color.fromARGB(255, 105, 144, 235),
+                              size: 20),
+                          Text("Contacto",
+                              style: TextStyle(
+                                  color: Color.fromARGB(255, 105, 144, 235))),
+                        ],
+                      ),
+                    ),
+                    Row(
+                      children: const [
+                        Text("Tel y Fax\n(662) 259.2155\nExt: 2482 y 2494",
+                            textAlign: TextAlign.left,
+                            style: TextStyle(
+                                color: Color.fromARGB(255, 255, 255, 255),
+                                fontSize: 10)),
+                      ],
+                    ),
+                    Row(
+                      children: const [
+                        Icon(Icons.wb_twilight_outlined,
+                            color: Color.fromARGB(255, 105, 144, 235),
+                            size: 40),
+                        Icon(Icons.facebook,
+                            color: Color.fromARGB(255, 105, 144, 235), size: 40)
+                      ],
+                    ),
+                  ],
+                )
+              ]),
+              Column(
+                children: [
+                  Container(
+                    margin: const EdgeInsets.only(right: 80),
+                    child: Row(
+                      children: const [
+                        Icon(Icons.location_on,
+                            color: Color.fromARGB(255, 105, 144, 235),
+                            size: 20),
+                        Text("Dirección",
+                            textAlign: TextAlign.end,
+                            style: TextStyle(
+                                color: Color.fromARGB(255, 105, 144, 235))),
+                      ],
+                    ),
+                  ),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.start,
                     children: const [
-                      Icon(Icons.phone,
-                          color: Color.fromARGB(255, 105, 144, 235), size: 20),
-                      Text("Contacto",
+                      Text(
+                          "Edificio 3K4, Ciencias\nde la computación,\nBoulevard Luis\nEncinas y Rosales s/n,\nCol.Centro,\nHermosillo,Sonora.\n CP 83000",
+                          textAlign: TextAlign.left,
                           style: TextStyle(
-                              color: Color.fromARGB(255, 105, 144, 235))),
+                              color: Color.fromARGB(255, 255, 255, 255),
+                              fontSize: 10)),
                     ],
                   ),
-                ),
-                Row(
-                  children: const [
-                    Text("Tel y Fax\n(662) 259.2155\nExt: 2482 y 2494",
-                        textAlign: TextAlign.left,
-                        style: TextStyle(
-                            color: Color.fromARGB(255, 255, 255, 255),
-                            fontSize: 10)),
-                  ],
-                ),
-                Row(
-                  children: const [
-                    Icon(Icons.wb_twilight_outlined,
-                        color: Color.fromARGB(255, 105, 144, 235), size: 40),
-                    Icon(Icons.facebook,
-                        color: Color.fromARGB(255, 105, 144, 235), size: 40)
-                  ],
-                ),
-              ],
-            )
-          ]),
-          Column(
-            children: [
-              Container(
-                margin: const EdgeInsets.only(right: 80),
-                child: Row(
-                  children: const [
-                    Icon(Icons.location_on,
-                        color: Color.fromARGB(255, 105, 144, 235), size: 20),
-                    Text("Dirección",
-                        textAlign: TextAlign.end,
-                        style: TextStyle(
-                            color: Color.fromARGB(255, 105, 144, 235))),
-                  ],
-                ),
-              ),
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: const [
-                  Text(
-                      "Edificio 3K4, Ciencias\nde la computación,\nBoulevard Luis\nEncinas y Rosales s/n,\nCol.Centro,\nHermosillo,Sonora.\n CP 83000",
-                      textAlign: TextAlign.left,
-                      style: TextStyle(
-                          color: Color.fromARGB(255, 255, 255, 255),
-                          fontSize: 10)),
                 ],
               ),
-            ],
-          ),
-          Column(
-            children: [
-              Container(
-                margin: const EdgeInsets.only(right: 120),
-                child: Row(
-                  children: const [
-                    Icon(Icons.mail,
-                        color: Color.fromARGB(255, 105, 144, 235), size: 20),
-                    Text("Correo",
-                        textAlign: TextAlign.end,
-                        style: TextStyle(
-                            color: Color.fromARGB(255, 105, 144, 235))),
-                  ],
-                ),
-              ),
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: const [
-                  Text("computacion@mat.uson.mx",
-                      textAlign: TextAlign.end,
-                      style: TextStyle(
-                          color: Color.fromARGB(255, 255, 255, 255),
-                          fontSize: 10)),
+              Column(
+                children: [
+                  Container(
+                    margin: const EdgeInsets.only(right: 120),
+                    child: Row(
+                      children: const [
+                        Icon(Icons.mail,
+                            color: Color.fromARGB(255, 105, 144, 235),
+                            size: 20),
+                        Text("Correo",
+                            textAlign: TextAlign.end,
+                            style: TextStyle(
+                                color: Color.fromARGB(255, 105, 144, 235))),
+                      ],
+                    ),
+                  ),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: const [
+                      Text("computacion@mat.uson.mx",
+                          textAlign: TextAlign.end,
+                          style: TextStyle(
+                              color: Color.fromARGB(255, 255, 255, 255),
+                              fontSize: 10)),
+                    ],
+                  ),
                 ],
               ),
-            ],
-          ),
-        ]),
+            ]),
       ),
     );
   }
