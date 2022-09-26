82136146d85f448092f8748cf71a05c14d6569f5
Sebastian Rivera <sebastan12grc@gmail.com>
2022-09-26 13:38:40 -0700

    Modelo Admin, Student y Subject

--- lib/models/admin/admin.dart
+++ lib/models/admin/admin.dart
@@ -1,6 +1,35 @@
-class AdminModel{
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-  //TODO: Add Admin properties
-  
-  //TODO Add Admin methods
+class AdminModel {
+  final int id_admin;
+  final String nombre;
+  final String correo;
+  final String password;
+  final Date fecha_Creacion; 
+
+  AdminModel({
+    required this.id_admin,
+    required this.nombre,
+    required this.correo,
+    required this.password,
+    required this.fecha_Creacion,
+  });
+
+  //alumno from map
+  factory AdminModel.fromMap(dynamic map) => AdminModel(
+    id_admin: map['id_admin'] as int,
+    nombre: map['nombre'] as String,
+    correo: map['correo'] as String,
+    password: map['password'] as String,
+    fecha_Creacion: map['fecha_Creacion'] as Date,
+  );
+
+    //alumno to map
+Map<String, dynamic> toMap() => {
+    'id_admin': id_admin,
+    'nombre': nombre,
+    'correo': correo,
+    'password': password,
+    'fecha_Creacion': fecha_Creacion,
+    };
 }
--- lib/models/subject/subject.dart
+++ lib/models/subject/subject.dart
@@ -1,6 +1,47 @@
-class SubjectModel{
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-  //TODO: Add Subject properties
-  
-  //TODO Add Subject methods
+class SubjectModel {
+  final int clave_mat;
+  final String asignatura;
+  final int creditos;
+  final int horas_teoria;
+  final int horas_lab;
+  final String division_academica;
+  final String departamento;
+  final String eje;
+
+  SubjectModel({
+    required this.clave_mat,
+    required this.asignatura,
+    required this.creditos,
+    required this.horas_teoria,
+    required this.horas_lab,
+    required this.division_academica,
+    required this.departamento,
+    required this.eje,
+  });
+
+  //alumno from map
+  factory SubjectModel.fromMap(dynamic map) => SubjectModel(
+    clave_mat: map['clave_mat'] as int,
+    asignatura: map['asignatura'] as String,
+    creditos: map['creditos'] as int,
+    horas_teoria: map['horas_teoria'] as int,
+    horas_lab: map['horas_lab'] as int,
+    division_academica: map['division_academica'] as String,
+    departamento: map['departamento'] as String,
+    eje: map['eje'] as String,
+  );
+
+    //alumno to map
+Map<String, dynamic> toMap() => {
+    'clave_mat': clave_mat,
+    'asignatura': asignatura,
+    'creditos': creditos,
+    'horas_teoria': horas_teoria,
+    'horas_lab': horas_lab,
+    'division_academica': division_academica,
+    'departamento': departamento,
+    'eje': eje,
+    };
 }
